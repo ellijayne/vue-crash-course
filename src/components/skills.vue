9158304ba738a909814cb18a602d33477eb2b22a
ejwillard <ejwillard@squiz.net>
2019-04-19 11:11:44 +1000

    pushing modified components

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -5,6 +5,13 @@
     <!-- can use JS in interpolation/curlies below like so: -->
 
     <div class="holder">
+
+      <form @submit.prevent="addSkill" >
+
+        <input type="text" placeholder="Enter a skill you have..." v-model="skill" >
+
+      </form>
+
       <ul>
         <li v-for="(data, index) in skills" :key='index'> {{data.skill}} </li><!--this is how we reference the skills object below-->
       </ul>
@@ -21,12 +28,21 @@ export default {
   name: 'skills',
   data() {
     return {
+      skill:'',
       skills: [
         {"skill": "Vue.js"},
         {"skill": "Frontend Developer"}
       ]
     }
+  },
+
+  methods: {
+    addSkill() {
+      this.skills.push({skill: this.skill})
+      this.skill ="";
+    }
   }
+
 }
 </script>
 
@@ -60,6 +76,15 @@ export default {
 
   .container {
     box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
   }
 
 </style>
