365c1bc3005655e13799ec9d1d970511c1cc0937
ellijayne <ellijayne.m.willard@gmail.com>
2019-04-19 14:09:41 +1000

    finalsed content

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template> <!--Template is where all our HTML lives and vue directives and where vue content wrappers go-->
   <div id="app">
-    <skills />
+
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view />
+
   </div>
 </template>
 
 <script> //Logic goes here
-import skills from './components/skills.vue'
+import Skills from './components/Skills.vue'
 
 export default {
   name: 'app',
   components: {
-    skills
+    Skills
   }
 }
 </script>
@@ -24,7 +31,8 @@ export default {
     display: grid;
     grid-template-rows: auto;
     justify-items: center;
-    align-items: center;
+    /* align-items: center; */
+    padding-top: 50px;
   }
   body, html {
     margin: 0;
@@ -32,5 +40,19 @@ export default {
   }
   #app {
     width: 50%;
+  }
+
+  nav {
+    padding: 20px 20px 20px 0;
+  }
+
+  nav a {
+    padding: 10px;
+    text-decoration: none;
+    background: #fff;
+    border-radius: 3px;
+    color: rgb(0, 110, 255);
+    font-weight: bold;
+    margin-right: 15px;
   }
 </style>
--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    {{name}}
+    
 
     <!-- can use JS in interpolation/curlies below like so: -->
 
@@ -8,12 +8,26 @@
 
       <form @submit.prevent="addSkill" >
 
-        <input type="text" placeholder="Enter a skill you have..." v-model="skill" >
+        <input type="text" placeholder="Enter a skill you have..." v-model="skill" v-validate="'min:5'" name="skill">
+
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+        </transition>
 
       </form>
 
+
+
       <ul>
-        <li v-for="(data, index) in skills" :key='index'> {{data.skill}} </li><!--this is how we reference the skills object below-->
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+        <li v-for="(data, index) in skills" :key='index'> 
+
+          {{data.skill}} <!--this is how we reference the skills object below-->
+
+          <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+
+          </li>
+        </transition-group>
       </ul>
 
       <p>These are the skills that you possess</p>
@@ -30,24 +44,34 @@ export default {
     return {
       skill:'',
       skills: [
-        {"skill": "Vue.js"},
+        {"skill": "Vue.js"}, 
         {"skill": "Frontend Developer"}
-      ]
+      ],
     }
   },
 
   methods: {
     addSkill() {
-      this.skills.push({skill: this.skill})
-      this.skill ="";
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({skill: this.skill})
+          this.skill = "";
+        } else {
+          console.log("not valid")
+        }
+      })
+    },
+    remove(id) {
+      this.skills.splice(id,1);
     }
   }
-
 }
 </script>
 
 
-<style scoped> 
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
   .holder {
     background: #fff;
@@ -85,6 +109,38 @@ export default {
     font-size: 1.3em;
     background-color: #323333;
     color: #687F7F;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  i {
+    float: right;
   }
 
 </style>
